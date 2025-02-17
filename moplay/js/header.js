62afedc5f7daf978739a33cecaa8af6e9b408b1c
melika <melikaramzani32@gmail.com>
2025-02-17 18:59:55 +0330

    Final change in moplay for icon back

--- moplay/js/header.js
+++ moplay/js/header.js
@@ -1,13 +1,10 @@
-function hideImge(){
-    const currentPage = window.location.pathname;
-    if(currentPage === "/moplay/index.html"){
-        document.getElementById("logo-dark-mode").style.display = "block";
-        document.getElementById("logo-light-mode").style.display = "block";
+function hideIcon(){
+    if(location.href === 'http://127.0.0.1:5500/moplay/index-2.html'){
+        document.getElementById("logo-mode").style.display = "block";
         document.getElementById("icon-back").style.display = "none";
     }else{
-         document.getElementById("logo-dark-mode").style.display = "none"
-         document.getElementById("logo-light-mode").style.display = "none";
+         document.getElementById("logo-mode").style.display = "none";
          document.getElementById("icon-back").style.display = "block";
-    }
-}
-hideImge();
+    };
+};
+hideIcon();
